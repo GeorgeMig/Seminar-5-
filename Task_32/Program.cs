a4120b2a35423cfef863bbe05cc2904da40bb511
George_M <msgoha555@yandex.ru>
2022-05-22 11:23:47 +0300

    Completed Task 32

---
+++ Task_32/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] nums = new int [12];
+Random numRnd = new Random ();
+for (int i = 0; i < nums.Length; i++)
+{
+    nums[i] = numRnd.Next(-9, 10);
+    Console.Write(nums [i] + " ");
+}
+
+Console.WriteLine();
+for (int i = 0; i < nums.Length; i++)
+{
+    nums [i] = nums [i] * - 1;
+    Console.Write (nums [i] + " ");
+}
