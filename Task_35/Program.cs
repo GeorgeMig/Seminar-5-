21acda0d136201b0d4eb5be015a16a1bc388a24c
George_M <msgoha555@yandex.ru>
2022-05-22 11:57:25 +0300

    Completed Task 35

---
+++ Task_35/Program.cs
@@ -0,0 +1,19 @@
+﻿int [] nums = new int [123];
+Random numRnd = new Random ();
+for (int i = 0; i < nums.Length; i++)
+{
+    nums[i] = numRnd.Next(0, 200);
+    Console.Write(nums [i] + " ");
+}
+
+Console.WriteLine();
+
+int count = 0;
+for (int i = 0; i < nums.Length; i++)
+{
+    if (nums [i] >= 10 && nums [i] <= 99)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Количество чисел = {count}");
