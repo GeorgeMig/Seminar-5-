f5d7fc2d076274dbdb4b46a16105a4c1259a654c
George_M <msgoha555@yandex.ru>
2022-05-22 11:44:41 +0300

    Completed Task 33

---
+++ Task_33/Program.cs
@@ -0,0 +1,19 @@
+﻿int [] nums = new int [12];
+Random numRnd = new Random ();
+for (int i = 0; i < nums.Length; i++)
+{
+    nums[i] = numRnd.Next(0, 10);
+    Console.Write(nums [i] + " ");
+}
+
+Console.Write("Введите число => ");
+int find = int.Parse(Console.ReadLine());
+for (int i = 0; i < nums.Length; i++)
+{
+    if (nums [i] == find)
+    {
+        Console.Write("Да");
+        break;
+    }
+    
+}
