4c8a2d0763f50f0972ed2f64f549ef0f62e38530
George_M <msgoha555@yandex.ru>
2022-05-22 12:29:35 +0300

    start Task 37

---
+++ Task_37/Program.cs
@@ -0,0 +1,19 @@
+﻿int [] nums = new int [123];
+Random numRnd = new Random ();
+for (int i = 0; i < nums.Length; i++)
+{
+    nums[i] = numRnd.Next(0, 200);
+    Console.Write(nums [i] + " ");
+}
+
+Console.WriteLine();
+
+
+void MultiArray (int [] num)
+{
+    for (int i = 0; i < num.Length; i++)
+    {
+        num[i] * num.Length - 1 - i;
+    }
+    
+}
