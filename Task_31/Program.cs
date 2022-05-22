13014d5a14c213a321faffd66717ac073ee06c6c
George_M <msgoha555@yandex.ru>
2022-05-22 11:01:26 +0300

    Completed Task 31

--- Task_31/Program.cs
+++ Task_31/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int [] nums = new int [12];
+Random numRnd = new Random ();
+for (int i = 0; i < nums.Length; i++)
+{
+    nums[i] = numRnd.Next(-9, 10);
+    Console.Write(nums [i] + " ");
+}
+
+int sumPositive = 0;
+int sumNegative = 0;
+for (int i = 0; i < nums.Length; i++)
+{
+    if(nums[i] <0) sumNegative += nums [i];
+    else sumPositive += nums [i];
+}
+Console.WriteLine();
+Console.WriteLine($"Сумма положительных = {sumPositive}");
+Console.WriteLine($"Сумма отрицательных = {sumNegative}");
