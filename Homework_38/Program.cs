670e60092057823c1ad06722d4389dea559ff483
George_M <msgoha555@yandex.ru>
2022-06-05 18:26:51 +0300

    Complete HW_38 + add name programm in HW_36

---
+++ Homework_38/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+int [] nums = new int [12];
+int max = 0;
+Random numRnd = new Random ();
+for (int i = 0; i < nums.Length; i++)
+{
+    nums[i] = numRnd.Next(1, 100);
+    Console.Write(nums [i] + " ");
+    if (nums [i] > max)
+    {
+        max = nums [i];
+    }
+}
+int min = max;
+for (int i = 0; i < nums.Length; i++)
+{
+    if (nums [i] < min)
+    {
+        min = nums [i];
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"Максимальный элемент = {max}, Минимальный элемент ={min}");
+Console.WriteLine($"Разница между ними = {max - min}");
