51580cb87fa45beccf593f53f49b172b844e0b29
George_M <msgoha555@yandex.ru>
2022-06-05 17:43:52 +0300

    Complete 34

---
+++ Homework_34/Program.cs
@@ -0,0 +1,19 @@
+﻿int [] nums = new int [12];
+Random numRnd = new Random ();
+for (int i = 0; i < nums.Length; i++)
+{
+    nums[i] = numRnd.Next(1, 100);
+    Console.Write(nums [i] + " ");
+}
+int count = 0;
+for (int i = 0; i < nums.Length; i++)
+{
+    if (nums[i] % 2 == 0) 
+    {
+        count++;
+    }
+}
+
+Console.WriteLine ($"Количество четных чисел в массиве = {count}");
+
+
